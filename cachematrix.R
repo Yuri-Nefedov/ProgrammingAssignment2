43d2b870cafb0069f73edc08eb8fe7356f2632ac
Yuri-Nefedov <eury@yandex.ru>
2017-01-16 00:37:12 +0300

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,30 @@
-## Put comments here that give an overall description of what your
-## functions do
+## _makeCacheMatrix creates a variable inv to store the inverse of incoming matrix in it
+## _cacheSolve checks if inverse calculated and just returns it; otherwise it calculates the inverse and transfer for storing 
 
-## Write a short comment describing this function
+## First function includes 3 inside: transferring matrix for calculations and two to store|return the inverse 
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL
+  if (det(x) == 0) message("Warning! D = 0. The matrix is singular!") 
+  get <- function() x                                         ## to transfer incoming matrix
+  setinverse <- function(inverse) inv <<- inverse             ## NB! to store calculated inverse
+  getinverse <- function() inv                                ## to return stored inverse 
+  list(get=get, setinverse=setinverse, getinverse=getinverse) ## return as a whole function result 
 }
 
 
-## Write a short comment describing this function
+## Check if inverse calculated | solve if not and store
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inv <- x$getinverse()             ## checking the inverse in cache 
+  if(!is.null(inv)) {               ## return if found & skip
+    message("getting cached data.")
+    return(inv)
+  }
+  data <- x$get()                   ## getting matrix from incoming function
+  if (det(data) == 0) message("Can't calculate singular matrix with D=0")
+  else                              ## inverting if possible
+    {inv <- solve(data)
+    x$setinverse(inv)               ## stroring by means of incoming function 
+    inv}                            ## return inverse 
 }
